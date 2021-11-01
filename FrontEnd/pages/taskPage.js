fd186929893f973082762aeec182107e5f960bce
Ricardo Rodriguez <ricardo.rodriguezr@wizeline.com>
2021-11-01 16:22:14 -0600

    update selectors

--- FrontEnd/pages/taskPage.js
+++ FrontEnd/pages/taskPage.js
@@ -10,7 +10,7 @@ class taskPage{
         this.addTaskBtn = Selector('button.reactist_button.reactist_button--primary')
         this.cancelBtn  = Selector('button.reactist_button.reactist_button--secondary')
         this.getAllTask = Selector('.task_list_item__content')
-        this.getTask = Selector('.task_list_item__info_tags')
+        this.getTask = Selector('span.task_list_item__project')
         this.getTaskName = Selector('.markdown_content.task_content')
         this.deleteTaskBtn = Selector('.menu_item.icon_menu_item.danger_menu')
         this.deleteConfirmationBtn = Selector('.reactist_modal_box__actions .ist_button.ist_button_red')
